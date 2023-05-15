8c238424164fa7834b111b221ee891032830c9ed
Felipe Andrade <felipeandrade448@gmail.com>
2023-05-15 15:20:38 -0300

    Alter configurations

--- src/webapi-serverless/Program.cs
+++ src/webapi-serverless/Program.cs
@@ -1,19 +1,48 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
-namespace webapi_serverless;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    public static void Main(string[] args)
+    c.SwaggerDoc("v1", new OpenApiInfo
     {
-        CreateHostBuilder(args).Build().Run();
-    }
+        Version = "v1",
+        Title = "Web API Serverless",
+        Description = "Exemplo de uma API com .NET, Serverless e CI/CD",
+    });
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-}
+    try
+    {
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
+    }
+    catch { }
+});
+
+var app = builder.Build();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("./swagger/v1/swagger.json", "example-dotnet-api-serverless");
+        c.RoutePrefix = string.Empty;
+    });
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
